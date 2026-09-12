3341bb04d8e4d1b23de0ec1c9c38f71770daff08
mcastilloperona <castillo.perona@gmail.com>
2026-09-12 16:56:53 +0200

    feat: añadir service worker PWA

---
+++ sw.js
@@ -0,0 +1,14 @@
+const VERSION = 'cdh-btt-pwa-v1';
+
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request));
+});
